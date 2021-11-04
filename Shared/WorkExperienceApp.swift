107203e38a624dd779fb6a65e9305c7159c767aa
Jasamrit Rahala <rahala.j@etoncollege.org.uk>
2021-11-04 10:42:38 +0000

    Initial Commit

---
+++ Shared/WorkExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkExperienceApp.swift
+//  Shared
+//
+//  Created by Rahala, Jasamrit (Coll) on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WorkExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

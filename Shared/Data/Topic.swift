a8fc12843bf2ba194716ee748fdf554940f25ba6
Jasamrit Rahala <rahala.j@etoncollege.org.uk>
2021-11-04 10:57:51 +0000

    Topic Enumerable

---
+++ Shared/Data/Topic.swift
@@ -0,0 +1,16 @@
+//
+//  Topic.swift
+//  WorkExperience
+//
+//  Created by Rahala, Jasamrit (Coll) on 04/11/2021.
+//
+
+import Foundation
+
+enum Topic{
+    case Accounting
+    case Engineering
+    case DataScience
+    case Investing
+}
+
